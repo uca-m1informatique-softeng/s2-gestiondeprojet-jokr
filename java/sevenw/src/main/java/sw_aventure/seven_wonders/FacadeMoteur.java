d0a2e709173757b182f8c4be76c4974061f63655
iBananos <56481216+iBananos@users.noreply.github.com>
2021-02-04 02:53:20 +0100

    Creation de l'interface FacadeMoteur #4

--- java/sevenw/src/main/java/sw_aventure/seven_wonders/FacadeMoteur.java
+++ java/sevenw/src/main/java/sw_aventure/seven_wonders/FacadeMoteur.java
@@ -4,32 +4,72 @@ import sw_aventure.joueur.Joueur;
 import sw_aventure.objetjeu.*;
 
 
-
+/**
+ * Interface servant de Facade Moteur dans l'optique d'une prochaine séparation entre le Moteur-de-jeu et les Joueurs-Clients
+ */
 public interface FacadeMoteur {
 
-    public static boolean permisDeConstruction(Carte carte, Inventaire sInventaire, Inventaire gauche, Inventaire droite, Plateau plateau) {
+    /**
+     * Méthode appelée par le client pour connaitre la potentielle possibilité de construction d'une carte.
+     * @param carte La carte à construire
+     * @param sInventaire son Inventaire
+     * @param gauche L'inventaire du joueur de gauche
+     * @param droite L'inventaire du joueur de droite
+     * @param plateau Le plateau de jeu
+     * @return un boolean vis-à-vis de la possibilité de construire la carte
+     */
+    static boolean permisDeConstruction(Carte carte, Inventaire sInventaire, Inventaire gauche, Inventaire droite, Plateau plateau) {
         Construction construction = new Construction();
         return construction.permisDeConstruction(carte, sInventaire, gauche, droite, plateau);
     }
 
-    public static boolean laConstructionViaDoublons(Carte carte ,Inventaire sInventaire,boolean print){
+    /**
+     * Permet de savoir si la carte est déjà en possession du joueur
+     * @param carte La carte à tester
+     * @param sInventaire L'inventaire du joueur
+     * @param print Affichage dans la console ( ici False )
+     * @return un boolean quant à la possession de la carte par le joueur
+     */
+    static boolean laConstructionViaDoublons(Carte carte ,Inventaire sInventaire,boolean print){
         Construction construction = new Construction();
-        return laConstructionViaDoublons(carte,sInventaire,print);
+        return construction.laConstructionViaDoublons(carte,sInventaire,print);
     }
 
-    public static int getAge(Plateau plateau){
+    /**
+     * Permet de connaitre l'Age actuel au cours de la partie
+     * @param plateau Le plateau de jeu
+     * @return L'Age actuel du jeu
+     */
+    static int getAge(Plateau plateau){
         return plateau.getAge();
     }
 
-    public static int getTour(Plateau plateau){
+    /**
+     * Permet de connaitre le numéro du tour de l'Age actuel de la partie
+     * @param plateau Le plateau de jeu
+     * @return Le numéro du tour actuel
+     */
+    static int getTour(Plateau plateau){
         return plateau.getTour();
     }
 
-    public static Joueur joueurDroit(Plateau plateau, Joueur joueur) {
+    /**
+     * Permet d'avoir accès au voisin de droite du joueur appelé en paramètre
+     * @param plateau Le plateau de Jeu
+     * @param joueur Le Joueur auquel on souhaite avoir le voisin de droite
+     * @return Le voisin de droite
+     */
+    static Joueur joueurDroit(Plateau plateau, Joueur joueur) {
         return plateau.joueurDroit(joueur);
     }
 
-    public static Joueur joueurGauche(Plateau plateau, Joueur joueur) {
+    /**
+     * Permet d'avoir accès au joueur à gauche du joueur appelé en paramètre
+     * @param plateau le plateau de Jeu
+     * @param joueur Le Joueur auquel on souhaite avoir le voisin de gauche
+     * @return Le voisin de gauche
+     */
+    static Joueur joueurGauche(Plateau plateau, Joueur joueur) {
         return plateau.joueurGauche(joueur);
     }
 }
