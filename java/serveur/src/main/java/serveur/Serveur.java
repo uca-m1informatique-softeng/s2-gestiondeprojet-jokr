f2945b512cc3111ce27d279858a724961522124f
fpoutre <kevin.levy@etu.univ-cotedazur.fr>
2021-02-15 11:13:06 +0100

    Changed IP of server

--- java/serveur/src/main/java/serveur/Serveur.java
+++ java/serveur/src/main/java/serveur/Serveur.java
@@ -11,6 +11,8 @@ import metier.*;
 import statistique.Statistique;
 import utils.affichage.LoggerSevenWonders;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -157,11 +159,11 @@ public class Serveur {
      * Methode du main
      * @param args argument du main
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         LoggerSevenWonders.init(true);
         Configuration config = new Configuration();
 
-        config.setHostname("127.0.0.1");
+        config.setHostname(InetAddress.getLocalHost().getHostName());
         config.setPort(8081);
         SocketIOServer server = new SocketIOServer(config);
 
