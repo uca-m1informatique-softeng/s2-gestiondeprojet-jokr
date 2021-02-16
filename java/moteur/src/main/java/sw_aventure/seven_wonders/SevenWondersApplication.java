40bbe0740e4bc1563af7f111f063278ebf1d389e
Ossama <ossama1998@windowslive.com>
2021-02-16 13:06:24 +0100

    Spring dépendances

--- java/moteur/src/main/java/sw_aventure/seven_wonders/SevenWondersApplication.java
+++ java/moteur/src/main/java/sw_aventure/seven_wonders/SevenWondersApplication.java
@@ -1,11 +1,11 @@
-package joueur;
+package sw_aventure.seven_wonders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JoueurApplication  {
+public class SevenWondersApplication {
     public static void main(String[] args){
-        SpringApplication.run(JoueurApplication.class,args);
+        SpringApplication.run(SevenWondersApplication.class,args);
     }
 }
