a6cfe0f31b0aa650177be1c56df9153672247c03
Ossama <ossama1998@windowslive.com>
2021-02-16 12:33:24 +0100

    Dépendances Spring #23

---
+++ java/client/src/main/java/joueur/JoueurApplication.java
@@ -0,0 +1,11 @@
+package joueur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoueurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class,args);
+    }
+}
