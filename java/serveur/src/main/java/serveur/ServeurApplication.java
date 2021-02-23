fdb630510f857aaaee6f7905fe980722548b29b9
fpoutre <kevin.levy@etu.univ-cotedazur.fr>
2021-02-23 18:01:33 +0100

    Correction de ServeurApplication
    #37

--- java/serveur/src/main/java/serveur/ServeurApplication.java
+++ java/serveur/src/main/java/serveur/ServeurApplication.java
@@ -13,18 +13,4 @@ public class ServeurApplication {
         SpringApplication.run(ServeurApplication.class,args);
         LoggerSevenWonders.init(true);
     }
-
-    @Bean
-    public CommandLineRunner aGame(@Autowired Serveur serveur) {
-        return args -> {
-            System.out.println("************************** aGame **************************************");
-
-            // pour faire la différence entre un lancement via les tests et un lancement par mvn exec:java@id
-            if (args.length >0) {
-                serveur.main(args);
-                //moteur.setExitOnFinish(true);
-
-            }
-        };
-    }
 }
