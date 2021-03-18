ce34bcf5a174f4ed603fb6a3340ebee1154ade97
iBananos <56481216+iBananos@users.noreply.github.com>
2021-03-19 00:28:03 +0100

    Correction coquille #39

--- java/moteur/src/main/java/sw_aventure/seven_wonders/SevenWonders.java
+++ java/moteur/src/main/java/sw_aventure/seven_wonders/SevenWonders.java
@@ -225,7 +225,7 @@ public class SevenWonders {
         for(int i = 0 ; i< inv.size() ; i++ ){
             webController.envoyerFin(inv.get(i));
         }
-        restTemplate.patchForObject(SevenWonders.statsServerURL + "/finir",null, Void.class);
+        restTemplate.postForObject(SevenWonders.statsServerURL + "/finir",null, Void.class);
 
         System.out.println("ON EST LAAAAAAAAAAAAAAA ON A ENVOYER LES STATS");
     }
