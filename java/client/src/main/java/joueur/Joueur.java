cc8d8d6b9ce39529eaf26bf2afe0e65d328365a3
Ossama <ossama1998@windowslive.com>
2021-03-19 20:12:40 +0100

    Update

--- java/client/src/main/java/joueur/Joueur.java
+++ java/client/src/main/java/joueur/Joueur.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "prototype")
+@Scope("singleton")
 public class Joueur {
     protected IA bot;
     protected String name;
