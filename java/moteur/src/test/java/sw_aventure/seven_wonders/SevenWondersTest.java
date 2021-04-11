b68d11a3b23c518c4a561b13320971dbb7c7a3ed
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2021-04-11 22:02:24 +0200

    Réparation test #40

--- java/moteur/src/test/java/sw_aventure/seven_wonders/SevenWondersTest.java
+++ java/moteur/src/test/java/sw_aventure/seven_wonders/SevenWondersTest.java
@@ -1,56 +1,78 @@
-/*
 package sw_aventure.seven_wonders;
 
-
-import exception.NegativeNumberException;
-import org.junit.Before;
-import org.junit.Test;
+import metier.EnumCarte;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import utilitaire_jeu.Inventaire;
 import utilitaire_jeu.Plateau;
 import utilitaire_jeu.SetInventaire;
 import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class SevenWondersTest {
 
+    @Autowired
     private SevenWonders sevenWonders;
+
     private SetInventaire setInv1, setInv2, setInv3;
-    private String joueur1, joueur2, joueur3;
-    private Plateau plateau;
 
     /**
      * Preparation des tests de la classe SevenWonders
      */
-/*
-    @Before
+    @BeforeEach
     public void setup() {
-        sevenWonders = new SevenWonders(3, false, false);
+        SevenWonders.args = new String[]{"true", "1", "3", "false", "http://127.0.0.1:8085/"};
+        sevenWonders = new SevenWonders();
+
 
         setInv1 = new SetInventaire( 1,"RHERBEN", "Enzo");
         setInv2 = new SetInventaire( 2,"EBREBEB", "Christina");
         setInv3 = new SetInventaire( 3,"GBREBEB", "Mona");
 
-
-        joueur1 = setInv1.getUrl();
-        joueur2 = setInv2.getUrl();
-        joueur3 = setInv3.getUrl();
+        String joueur1 = setInv1.getUrl();
+        String joueur2 = setInv2.getUrl();
+        String joueur3 = setInv3.getUrl();
 
         ArrayList<Inventaire> listeInventaire = new ArrayList<>(){{add(setInv1);add(setInv2);add(setInv3);}};
-        plateau = new Plateau(listeInventaire);
+        Plateau plateau = new Plateau(listeInventaire);
+    }
 
 
+    @Test
+    public void getListeCarteToStringTest() {
+        List<EnumCarte> listCarte = new ArrayList<>();
+        listCarte.add(EnumCarte.M1);
+        listCarte.add(EnumCarte.J5);
+        listCarte.add(EnumCarte.B9);
+        List<String> listNomCarte = sevenWonders.getListeCarteToString(listCarte);
 
+        List<String> listCarteAttendue = new ArrayList<>();
+        listCarteAttendue.add("Exploitation Forestière");
+        listCarteAttendue.add("Vignoble");
+        listCarteAttendue.add("Jardins");
+
+        for (String carte : listNomCarte) {
+            assertTrue(listCarteAttendue.contains(carte));
+        }
     }
 
 
     /**
      * Test de la méthode initPlayers()
      */
-/*
+    /*
     @Test
     public void initPlayersTest() {
-        SevenWonders sw = new SevenWonders(0, false,false);
+        SevenWonders sw = new SevenWonders();
 
         assertEquals(0, sw.inv.size());
 
@@ -68,27 +90,26 @@ public class SevenWondersTest {
 
         sw.initPlayers(7,false);
         assertEquals(7, sw.inv.size());
-
-    }
+    }*/
 
 
     /**
      * Test de la méthode initPlateau()
      */
-/*
+    /*
     @Test
     public void initPlateauTest() {
         Plateau plateau = sevenWonders.initPlateau();
         assertEquals(3, plateau.getListeInventaire().size());
     }
+     */
 
 
     /**
      * Test de la méthode attributionMerveille()
      */
-/*
     @Test
-    public void attributionMerveilleTest() throws NegativeNumberException {
+    public void attributionMerveilleTest() {
         // Au départ les joueurs n'on pas de merveille donc NULL
         for (SetInventaire s : sevenWonders.inv) {
             assertNull(s.getMerveille());
@@ -102,4 +123,3 @@ public class SevenWondersTest {
     }
 }
 
-*/
